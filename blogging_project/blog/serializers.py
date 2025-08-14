c8306709f9b9c28be0b893e83846859a8c58170f
aditya-madwal <madwaladitya@gmail.com>
2025-08-14 18:48:47 +0530

    update : serializers fields set to manual orm

--- blogging_project/blog/serializers.py
+++ blogging_project/blog/serializers.py
@@ -2,13 +2,20 @@ from rest_framework import serializers
 from .models import Blog, Like, Comment
 
 class BlogSerializer(serializers.ModelSerializer):
-    likes_count = serializers.IntegerField(source="likes.count", read_only=True)
-    comments_count = serializers.IntegerField(source="comments.count", read_only=True)
+    likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Blog
         fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at', 'likes_count', 'comments_count']
         read_only_fields = ['author']
+
+    def get_likes_count(self, obj):
+        return Like.objects.filter(blog=obj).count()
+
+    def get_comments_count(self, obj):
+        return Comment.objects.filter(blog=obj).count()
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,11 +23,26 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'content', 'created_at']
         read_only_fields = ['user']
 
+
 class BlogDetailSerializer(serializers.ModelSerializer):
-    likes_count = serializers.IntegerField(source="likes.count", read_only=True)
-    comments_count = serializers.IntegerField(source="comments.count", read_only=True)
-    latest_comments = CommentSerializer(source='comments.all', many=True, read_only=True)
+    likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+    latest_comments = serializers.SerializerMethodField()
 
     class Meta:
         model = Blog
-        fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at', 'likes_count', 'comments_count', 'latest_comments']
+        fields = [
+            'id', 'author', 'title', 'content',
+            'created_at', 'updated_at',
+            'likes_count', 'comments_count', 'latest_comments'
+        ]
+
+    def get_likes_count(self, obj):
+        return Like.objects.filter(blog=obj).count()
+
+    def get_comments_count(self, obj):
+        return Comment.objects.filter(blog=obj).count()
+
+    def get_latest_comments(self, obj):
+        latest = Comment.objects.filter(blog=obj).order_by('-created_at')[:5]
+        return CommentSerializer(latest, many=True).data
